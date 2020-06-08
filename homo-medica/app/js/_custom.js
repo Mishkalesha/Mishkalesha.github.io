20c93c6c76c1627578b3ac3d220d486ff13184da
Turchynyk Oleksii <mishkalesha1994@gmail.com>
2020-06-08 15:29:42 +0300

    create mobile adaptive with homo-medica

--- homo-medica/app/js/_custom.js
+++ homo-medica/app/js/_custom.js
@@ -4,12 +4,51 @@ document.addEventListener("DOMContentLoaded", function () {
   const headerHeight = document.querySelector(".header").offsetHeight;
   const headerSubmenus = document.querySelectorAll(".header__submenu");
   const homeForm = document.querySelector(".socials__form");
+  const hamburger = document.querySelector(".hamburger");
+  const mobileMenu = document.querySelector(".header__mobile_menu");
+  const mobileMenuLink = document.querySelectorAll(".mobile__menu_link");
+  const mobileMenuClose = document.querySelector(".mobile__menu_close");
+  const docBody = document.querySelector("body");
+
+  function showMobileMenu() {
+    mobileMenu.style.visibility = "visible";
+    mobileMenu.style.opacity = 1;
+    mobileMenu.style.transform = "translateX(0)";
+  }
+
+  function hideMobileMenu() {
+    mobileMenu.style.visibility = "hidden";
+    mobileMenu.style.opacity = 0;
+    mobileMenu.style.transform = "translateX(100%)";
+  }
+
+  hamburger.addEventListener("click", function () {
+    this.classList.toggle("is-active");
+    if (this.classList.contains("is-active")) {
+      showMobileMenu();
+    } else {
+      hideMobileMenu();
+    }
+  });
+
+  mobileMenuClose.addEventListener("click", () => {
+    hideMobileMenu();
+    hamburger.classList.remove("is-active");
+  });
 
   headerSubmenus.forEach((item) => {
     item.style.top = `${headerHeight - 20}px`;
   });
 
+  mobileMenuLink.forEach((item) => {
+    item.addEventListener("click", () => {
+      hamburger.classList.remove("is-active");
+      hideMobileMenu();
+    });
+  });
+
   homeForm.addEventListener("submit", (e) => e.preventDefault());
+
   document.addEventListener("scroll", () => {
     if (window.pageYOffset > headerHeight) {
       header.classList.add("header__fixed");
@@ -17,11 +56,36 @@ document.addEventListener("DOMContentLoaded", function () {
       header.classList.remove("header__fixed");
     }
   });
+
   $(".doctors__container").slick({
     dots: true,
     slidesToShow: 4,
     infinite: true,
     autoplay: true,
     autoplaySpeed: 3000,
+    arrows: false,
+
+    //variableWidth: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          centerMode: true,
+        },
+      },
+    ],
   });
 });
